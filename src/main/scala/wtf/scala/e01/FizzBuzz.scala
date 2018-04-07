0c0bc696e8341a9e3efd7e0c3327e6d47ba3057c
Dmitry.K <steklopod@gmail.com>
2018-04-07 21:56:16 +0300

    Первая попытка

--- src/main/scala/wtf/scala/e01/FizzBuzz.scala
+++ src/main/scala/wtf/scala/e01/FizzBuzz.scala
@@ -1,21 +1,34 @@
 package wtf.scala.e01
 
 object FizzBuzz {
-
   /**
     * Return "fizz", "buzz", "fizzbuzz" or number between 0 (inclusive) and n (exclusive).
     * For a given natural number greater than zero return:
-    *   "fizzbuzz" if the number is dividable by 3 and 5
-    *   "buzz" if the number is dividable by 5
-    *   "fizz" if the number is dividable by 3
+    * "fizzbuzz" if the number is dividable by 3 and 5
+    * "buzz" if the number is dividable by 5
+    * "fizz" if the number is dividable by 3
     * the same number if no other requirement is fulfilled
+    *
     * @param n
     * @throws IllegalArgumentException if n < 1
     * @return seq of fizzbuzz strings
     */
   @throws[IllegalArgumentException]
   private[e01] def fizzBuzzUntil(n: Int): Seq[String] = {
-    ???
+    require(n > 0, "Должно быть положительным")
+
+    def devideThree(number: Int) = {
+      if (number % 3 == 0) "fizz"
+      else ""
+    }
+    def devideFive(number: Int) = {
+      if (number % 5 == 0) "buzz"
+      else ""
+    }
+    var answer = devideThree(n) + devideFive(n)
+    if(answer == "") Array(n.toString)
+    else
+    Array(devideThree(n), devideFive(n))
   }
 
   def main(args: Array[String]): Unit = {
--- src/main/scala/wtf/scala/e01/IntegerRoot.scala
+++ src/main/scala/wtf/scala/e01/IntegerRoot.scala
@@ -5,12 +5,19 @@ object IntegerRoot {
   /**
     * Assume x is non negative integer.
     * If x = y * y for some integer y, then return y, otherwise return -1
+    *
     * @param x non negative integer to calculate square root
     * @throws IllegalArgumentException if negative number passed (Hint: check require method)
     * @return -1 if x is not square of some integer
     *         square root of x if x is square of some integer
     */
   @throws[IllegalArgumentException]
-  def calculateRoot(x: Int): Int = ???
+  def calculateRoot(x: Int): Int = {
+    require(x >= 0, "Должно быть положительным")
+    val sqrt = math.sqrt(x)
+    if (sqrt % 1 == 0)
+      sqrt.toInt
+    else -1
+  }
 
 }
