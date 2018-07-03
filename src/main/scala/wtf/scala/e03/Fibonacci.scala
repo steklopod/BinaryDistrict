a96a88b48b240dcb1f28e89cdb4282c94df1534a
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-07-03 16:23:24 +0300

    level e03 is done

--- src/main/scala/wtf/scala/e03/Fibonacci.scala
+++ src/main/scala/wtf/scala/e03/Fibonacci.scala
@@ -1,5 +1,7 @@
 package wtf.scala.e03
 
+import scala.collection.mutable
+
 object Fibonacci {
 
   /**
@@ -7,10 +9,16 @@ object Fibonacci {
     * https://en.wikipedia.org/wiki/Fibonacci_number
     *
     * We assume that fib(1) = 0, fib(2) = 1, and so on
+    *
     * @param n
     * @return
     */
-  def fibRecursive(n: Int): Long = ???
+  def fibRecursive(n: Int): Long = n match {
+    case n if n == 1 => 0
+    case n if n == 2 => 1
+    case n => fibRecursive(n -1) + fibRecursive(n -2)
+
+  }
 
   /**
     * Compute the n th Fibonacci number using memoization
@@ -19,9 +27,13 @@ object Fibonacci {
     * We assume that fib(1) = 0, fib(2) = 1, and so on
     *
     * Hint: to store computed Fibonacci numbers you can use scala.collection.mutable.HashMap[Int, Long]
-    * @param n
+    *
     * @return
     */
-  def fibMemo(n: Int): Long = ???
+
+  private val storage = mutable.HashMap.empty[Int, Long]
+  def fibMemo(n: Int): Long = {
+    storage.getOrElseUpdate(n, fibRecursive(n))
+  }
 
 }
--- src/main/scala/wtf/scala/e03/FunctionSumming.scala
+++ src/main/scala/wtf/scala/e03/FunctionSumming.scala
@@ -7,12 +7,19 @@ object FunctionSumming {
     * You should use tail recursion in your implementation
     *
     * Example: Assume we have segment [0,2] and function f = x
-    *          Then sum(f)(0, 2) = f(0) + f(1) + f(2) = 0 + 1 + 2
+    * Then sum(f)(0, 2) = f(0) + f(1) + f(2) = 0 + 1 + 2
+    *
     * @param f - function to sum over segment
     * @param a - left bound of segment
     * @param b - right bound of segment
     * @return
     */
-  def sumFunction(f: Int => Double)(a: Int, b: Int): Double = ???
+  def sumFunction(f: Int => Double)(a: Int, b: Int): Double = {
+    if (a == b) {
+      f(a)
+    } else {
+      f(a) + sumFunction(f)(a + 1, b)
+    }
+  }
 
 }
